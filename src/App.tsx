d483f8e30058bdcec3658d3db95c8cc53920e0e3
Sany655 <mazharulalam26@gmail.com>
2022-03-17 05:24:38 +0600

    ready boilerplate

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,6 @@
-import React, { useCallback, useReducer, useRef } from 'react';
-import logo from './logo.svg';
-import './App.css';
-import TsReducer from './components/TsReducer';
-
 function App() {
   return (
-    <TsReducer />
+    <h1>Hello world with typescript</h1>
   );
 }
 
